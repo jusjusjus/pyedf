8ccb392924e75a66301d91d42db789bdb63871e0
Justus Schwabedal <jschwabedal@gmail.com>
2016-02-08 11:14:26 +0100

    derivation.py combines recording and montages.

---
+++ derivation.py
@@ -0,0 +1,50 @@
+#! /usr/bin/python
+
+import numpy as np
+from recording import recording
+from montages import montage
+import xml.etree.ElementTree as xml
+
+
+
+class derivation(recording):
+
+	def __init__(self, edf_filename, montage_filename):
+		recording.__init__(self, edf_filename)
+		self.mtg = montage(montage_filename)
+
+		self.channelindices = [self.channelnames.index(channel)
+					for channel in self.mtg.channels]			# Find the recording index of channels in the montage
+			
+		self.mixer = self.mtg.mixing_matrix(self.mtg.channels)
+
+
+	def get_data(self, state_of_interest=None, start=None, end=None, duration=None):
+		X = recording.get_data(self, state_of_interest, start, end, duration, self.channelindices)
+		return np.dot(np.transpose(self.mixer[0]), X)
+
+
+
+
+
+
+if __name__ == "__main__":
+
+	import pylab
+
+	dx = derivation('example/sample.edf', 'example/sample.mtg')
+
+
+	x = dx.get_data(start=0., duration=100.)
+
+	
+	for i in xrange(x.shape[0]):
+		pylab.plot(x[i]-30.*i)
+
+	pylab.show()
+
+
+
+
+
+
