658fd85a3bbf01fde020ef28a35596d31c04b274
Justus Schwabedal <jschwabedal@gmail.com>
2016-03-12 15:03:25 +0100

    Updated setup.py information.

--- setup.py
+++ setup.py
@@ -12,10 +12,20 @@ edf_module = setuptools.Extension('pyedf/recording/lib/_edf',
 
 setuptools.setup(
 	name = "pyedf",
-	version = "0.0.2",
+	version = "0.0.1",
 	author = "Justus Schwabedal",
-	license = "GPL",
+	author_email = "JSchwabedal@gmail.com",
+	description = ("Python-bindings to the edf data format."),
+	license = "GPLv2",
+	keywords = "edf",
+	url = "https://github.com/jusjusjus/pyedf",
 	packages = ['pyedf', 'pyedf/recording', 'pyedf/derivation', 'pyedf/score'],
-	ext_modules = [edf_module])
+	ext_modules = [edf_module],
+	classifiers = ["Development Status :: 4 - Beta",
+			"Intended Audience :: Science/Research",
+			"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+			"Programming Language :: C",
+			"Programming Language :: Python :: 2.7",
+			"Topic :: Scientific/Engineering :: Bio-Informatics"])
 
 
