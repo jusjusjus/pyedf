a4bbd48ab6046bc05b849eb29d00d1eb74ca0faf
Justus Schwabedal <jschwabedal@gmail.com>
2016-03-21 13:57:17 +0100

    read_md5 should be imported per default.

--- pyedf/__init__.py
+++ pyedf/__init__.py
@@ -2,7 +2,7 @@
 
 print "# Welcome to the 'European Data Format'"
 
-from recording import recording
+from recording import recording, read_md5
 from derivation import derivation, montage
 from score import event, state, score, mystrtime, interval2state
 
