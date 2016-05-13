a30ab7db86b7cd6cc6b8bff8436eb50ab28930d4
Justus Schwabedal <jschwabedal@gmail.com>
2016-05-13 11:36:07 +0200

    Event checks different input timestring formats.

--- pyedf/score/event.py
+++ pyedf/score/event.py
@@ -6,10 +6,80 @@ import datetime
 
 
 
+def strtime_format1(timestring):
+
+	if len(timestring) < 25:
+		return None
+
+	if timestring[4] == '-' and timestring[7] == '-' and timestring[10] == 'T' and timestring[13] == ':' and timestring[16] == ':' and timestring[19] == '.':
+		return datetime.datetime.strptime(timestring[:25], "%Y-%m-%dT%H:%M:%S.%f")
+
+	else:
+		return None
+
+
+
+def strtime_format2(timestring):
+
+	if len(timestring) < 19:
+		return None
+
+	if timestring[4] == '-' and timestring[7] == '-' and timestring[10] == 'T' and timestring[13] == ':' and timestring[16] == ':':
+		return datetime.datetime.strptime(timestring[:19], "%Y-%m-%dT%H:%M:%S")
+
+	else:
+		return None
+
+
+
+def strtime_format3(timestring):
+
+	if len(timestring) < 25:
+		return None
+
+	if timestring[2] == '/' and timestring[5] == '/' and timestring[10] == ' ' and timestring[13] == ':' and timestring[16] == ':' and timestring[19] == '.':
+		return datetime.datetime.strptime(timestring[:25], "%m/%d/%Y %H:%M:%S.%f")
+
+	else:
+		return None
+
+
+
+def strtime_format4(timestring):
+
+	if len(timestring) < 19:
+		return None
+
+	if timestring[2] == '/' and timestring[5] == '/' and timestring[10] == ' ' and timestring[13] == ':' and timestring[16] == ':':
+		return datetime.datetime.strptime(timestring[:19], "%m/%d/%Y %H:%M:%S")
+
+	else:
+		return None
+
+
+
+strtime_formats = [strtime_format1,
+		   strtime_format2,
+		   strtime_format3,
+		   strtime_format4]
+
+
+
 def mystrtime(timestring):
 
-	try:	 return datetime.datetime.strptime(timestring[:25], "%Y-%m-%dT%H:%M:%S.%f")
-	except:	 return datetime.datetime.strptime(timestring[:19], "%Y-%m-%dT%H:%M:%S")
+	dtime = None
+
+	for strtime in strtime_formats:
+		dtime = strtime( timestring )
+	
+		if not dtime == None:
+			return dtime
+	
+	if dtime == None:
+		print "mystrtime::error : Unknown time string '"+timestring+"'"
+
+	return None
+
 
 
 
